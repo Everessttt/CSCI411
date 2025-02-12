1aca4ad2b9f6bfdf443ca4ca549cb53bd21ceeaf
Everessttt <everestsky76@gmail.com>
2025-02-12 11:13:09 -0800

    first commit

---
+++ assignment1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main() {
+    mario_kart_rankings();
+    makeSCCs(vector<vector<int>> G);
+
+    return 0;
+}
+
+void mario_kart_rankings() {
+    class kart_graph {
+        public:
+            kart_graph();
+
+
+    };
+}
